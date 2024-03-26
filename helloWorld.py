ebc6364463bb20d4dac1669610967fafeb410b89
Ana Magalhaes <anaimmags@gmail.com>
2024-03-26 20:01:32 +0000

    Added comments to improve code readability

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,9 @@
+# Prompt the user to enter a message
 message = input("Enter your message:")
 
-print(message)
+# Check if the input is empty
+if not message:
+    print("Error: No message entered.")
+else:
+    # Print the entered message
+    print(message)
